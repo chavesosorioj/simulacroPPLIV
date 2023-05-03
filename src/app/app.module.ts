b65d061e9b7cee1d4cacb8dea3e9aa5410960e06
Juliana Chaves <50085161+juchita@users.noreply.github.com>
2023-05-03 14:10:17 -0300

    agrego firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,12 @@ import { BusquedaComponent } from './componentes/busqueda/busqueda.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BienvenidoComponent } from './componentes/bienvenido/bienvenido.component';
 import { ShareModule } from './share/share.module';
+import { LoginComponent } from './componentes/login/login.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
@@ -14,12 +20,13 @@ import { ShareModule } from './share/share.module';
     DetallePeliculaComponent,
     BusquedaComponent,
     BienvenidoComponent,
+    LoginComponent,
    // MenuComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule, 
-    ShareModule,
+    ShareModule, provideFirebaseApp(() => initializeApp(environment.firebase)), provideAuth(() => getAuth()), provideDatabase(() => getDatabase()), provideFirestore(() => getFirestore()),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'simulacroprimerparcial-chaves',
+    appId: '1:11352775214:web:6b091aee3e8fc4528e9dbf',
+    storageBucket: 'simulacroprimerparcial-chaves.appspot.com',
+    apiKey: 'AIzaSyA4CiHEy71nlCc3_snTMCHxfcth1_L5CJ8',
+    authDomain: 'simulacroprimerparcial-chaves.firebaseapp.com',
+    messagingSenderId: '11352775214',
+  },
   production: true
 };
